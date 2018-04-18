85bc5525ff6f8e26b754d8b62a14ef26da1fb19c
mtmiro5823 <38364140+mtmiro5823@users.noreply.github.com>
2018-04-18 19:06:58 -0400

    Require database for loader.bat command
    Require entry of the database name to the load.bat command.  For example:
    
    C:\loader.bat mydb

--- Loader.bat
+++ Loader.bat
@@ -1,10 +1,7 @@
 rem sets the PGADMIN password
 set PGPASSWORD=geografio
 
-rem automatically assigns the appropriate directory
-
 rem run psql
-"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -U postgres -f craters.sql  
-"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -U postgres -f IAU2000.sql
-
-
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -U postgres -c "CREATE DATABASE %1;" 
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -U postgres -f craters.sql -d %1  
+"C:\Program Files\PostgreSQL\9.6\bin\psql.exe" -U postgres -f IAU2000.sql -d %1
